f4a56f380e1611032f579592aeb11ac66520fa33
Flavio F <flavio.filho@hotmail.com>
2023-10-21 10:52:05 -0300

    Removendo erros de console

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -4,9 +4,9 @@ export default async function Dashboard() {
   const { user }: any = await getSession()
   const people = [
     { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Guilherme Bao',  title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Professor Wilson', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Raduque Jeremias', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+    { name: 'Guilherme Bao',  title: 'Front-end Developer', email: 'lindsay.walton@example2.com', role: 'Member' },
+    { name: 'Professor Wilson', title: 'Front-end Developer', email: 'lindsay.walton@example3.com', role: 'Member' },
+    { name: 'Raduque Jeremias', title: 'Front-end Developer', email: 'lindsay.walton@example4.com', role: 'Member' },
     // More people...
   ]
 
