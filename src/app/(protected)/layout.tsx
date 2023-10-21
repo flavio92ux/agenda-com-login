c2820cd0b76a98c9ecc183b34d0299f2e3e4c0ba
Flavio F <flavio.filho@hotmail.com>
2023-10-21 14:01:57 -0300

    Adicionando metadados

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,9 +1,15 @@
 import { getSession } from '@auth0/nextjs-auth0'
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"
+
 import NavBar from '@/app/components/NavBar'
 
 interface IProp {
   children: React.ReactNode
+}
+
+export const metadata = {
+  title: 'App de agenda',
+  description: 'Programação de agenda',
 }
 
 export default async function AdminLayout({ children }: IProp) {
