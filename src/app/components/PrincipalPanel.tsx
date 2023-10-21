059ea7882743d40eeccfc1c6a4abaed753eff05d
Flavio F <flavio.filho@hotmail.com>
2023-10-21 15:58:36 -0300

    Tentativa modal

--- src/app/components/PrincipalPanel.tsx
+++ src/app/components/PrincipalPanel.tsx
@@ -15,19 +15,15 @@ export default function PrincipalPanel({ people }: IPrincipalPanel) {
       <div className='sm:flex sm:items-center'>
         <div className='sm:flex-auto'>
           <h1 className='text-base font-semibold leading-6 text-gray-900'>
-            Users
+            Lista de clientes
           </h1>
-          <p className='mt-2 text-sm text-gray-700'>
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
         </div>
         <div className='mt-4 sm:ml-16 sm:mt-0 sm:flex-none'>
           <button
             type='button'
             className='block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
           >
-            Add user
+            Adicionar usuário
           </button>
         </div>
       </div>
@@ -42,25 +38,19 @@ export default function PrincipalPanel({ people }: IPrincipalPanel) {
                       scope='col'
                       className='py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6'
                     >
-                      Name
+                      Nome
                     </th>
                     <th
                       scope='col'
                       className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
                     >
-                      Title
+                      Endereço
                     </th>
                     <th
                       scope='col'
                       className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
                     >
-                      Email
-                    </th>
-                    <th
-                      scope='col'
-                      className='px-3 py-3.5 text-left text-sm font-semibold text-gray-900'
-                    >
-                      Role
+                      Telefone
                     </th>
                     <th
                       scope='col'
@@ -82,15 +72,12 @@ export default function PrincipalPanel({ people }: IPrincipalPanel) {
                       <td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
                         {person.email}
                       </td>
-                      <td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
-                        {person.role}
-                      </td>
                       <td className='relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6'>
                         <a
                           href='#'
                           className='text-indigo-600 hover:text-indigo-900'
                         >
-                          Edit<span className='sr-only'>, {person.name}</span>
+                          Editar
                         </a>
                       </td>
                     </tr>
