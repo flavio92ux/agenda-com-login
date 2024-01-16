95c480269d6adbb20b25e905886f114088945ffe
Flavio F <flavio.filho@hotmail.com>
2024-01-16 20:23:36 -0300

    Oi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Index() {
   const session = await getSession()
 
   if (session) {
-    redirect('/dashboard') //
+    redirect('/dashboard')
   }
 
   redirect('/api/auth/login')
